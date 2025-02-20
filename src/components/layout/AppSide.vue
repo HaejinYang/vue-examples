9b0728ef8a72808a5214d5a09695103ada1812ad
haejin.yang91 <haejin.yang91@gmail.com>
2025-02-20 15:13:04 +0900

    Vue 예제를 따라가는 김에 사이드 및 컨텐츠 부위도 따라감.
    사이드에서 예제를 볼 수 있는 구조룰 구축하고, 색깔을 맞춤. 컨텐츠에선 레이아웃 위주로 조절함

--- src/components/layout/AppSide.vue
+++ src/components/layout/AppSide.vue
@@ -1,25 +1,88 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const id = ref(0)
+
+const basicRoutes = [
+  { path: '/hellovue', name: '안녕 Vue', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+]
+
+const practiveRoutes = [
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+  { path: '/markdown', name: 'Markdown 편집기', id: id.value++ },
+]
+
+const activeRouterId = ref(-1)
+const setActive = (id: number) => {
+  activeRouterId.value = id
+}
+</script>
 
 <template>
   <div class="side">
-    <RouterLink to="/markdown" class="link">markdown</RouterLink>
-    <RouterLink to="/" class="link">123</RouterLink>
-    <RouterLink to="/" class="link">123</RouterLink>
-    <RouterLink to="/" class="link">123</RouterLink>
+    <hr class="horizontal-line" />
+    <div class="side-container">
+      <p>기초</p>
+      <div class="side-item" v-for="route in basicRoutes" :key="route.id">
+        <RouterLink
+          :to="route.path"
+          :class="{ active: activeRouterId == route.id, router: true }"
+          @click="setActive(route.id)"
+        >
+          {{ route.name }}
+        </RouterLink>
+      </div>
+    </div>
+
+    <div class="side-container">
+      <p>실습</p>
+      <div class="side-item" v-for="route in practiveRoutes" :key="route.id">
+        <RouterLink
+          :to="route.path"
+          :class="{ active: activeRouterId == route.id, router: true }"
+          @click="setActive(route.id)"
+        >
+          {{ route.name }}
+        </RouterLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .side {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100px;
-  background-color: antiquewhite;
-  padding-top: 5px;
+  width: 250px;
+
+  .side-container {
+    padding: 20px 20px 0px 20px;
+
+    .side-item {
+      padding-top: 5px;
+    }
+  }
 }
-.link {
-  color: black;
-  padding: 5px;
+
+.router {
+  color: var(--weak-string-color);
+  text-decoration: none;
+  transition: color 0.3s ease-in-out;
+
+  &:not(.active):hover {
+    color: black;
+  }
+}
+
+.active {
+  color: var(--primary-string-color);
+}
+
+.horizontal-line {
+  border-top: 1px solid var(--weak-line-color);
+  margin: 40px 20px 0 20px;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MarkDownView from '@/views/MarkDownView.vue'
+import HelloVue from '@/views/HelloVue.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/markdown',
       name: 'markdown',
       component: MarkDownView,
+    },
+    {
+      path: '/hellovue',
+      name: 'hellovue',
+      component: HelloVue,
     },
     // {
     //   path: '/about',
