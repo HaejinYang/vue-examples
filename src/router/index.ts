458439c8bc1f52f8db47325b162ee318addfaa48
haejin.yang91 <haejin.yang91@gmail.com>
2025-02-20 15:15:39 +0900

    HelloVue 컴포넌트 이름을 HelloVueView로 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MarkDownView from '@/views/MarkDownView.vue'
-import HelloVue from '@/views/HelloVue.vue'
+import HelloVue from '@/views/HelloVueView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
